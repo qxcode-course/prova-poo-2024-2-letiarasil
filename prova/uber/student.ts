282a2e67a5f2038be72d4d298f9877b65233563b
Letícia de Araújo <leticiade.araujo@alu.ufc.br>
2024-12-06 13:24:08 +0000

    conseguiii a moto

--- prova/uber/student.ts
+++ prova/uber/student.ts
@@ -3,20 +3,144 @@ function input(): string { let X: any = input; X.L = X.L || require("fs").readFi
 function write(text: any, endl="\n") { process.stdout.write("" + text + endl); }
 export {};
 
+class Pessoa {
+    private nome: string;
+    private dinheiro: number;
+
+    constructor(nome: string, dinheiro: number)  {
+        this.nome = nome;
+        this.dinheiro = dinheiro;
+    }
+
+    getNome() : string {
+        return this.nome;
+    }
+
+    getDinheiro() : number {
+        return this.dinheiro;
+    }
+
+    setDinheiro(valor: number) : void {
+        this.dinheiro = valor;
+    }
+
+    toString() : string {
+        return `${this.nome}:${this.dinheiro}`;
+    }
+}
+
+class Moto {
+    private custo: number;
+    private motorista: Pessoa | null;
+    private passageiro: Pessoa | null;
+
+    constructor() {
+        this.custo = 0;
+        this.motorista = null;
+        this.passageiro = null;
+    }
+
+    setMotorista (motorista: Pessoa) : boolean {
+        if (this.motorista != null) {
+            return false;
+        }
+        this.motorista = motorista;
+        return true;
+    }
+
+    setPassageiro (passageiro: Pessoa) : boolean {
+        if (this.motorista == null) {
+            return false;
+        }
+        if (this.passageiro != null) {
+            return false;
+        }
+        this.passageiro = passageiro;
+        return true;
+    }
+
+    dirigir (km: number) : void {
+        if (this.motorista == null || this.passageiro == null) {
+            return;
+        }
+        this.custo += km;
+        return;
+    }
+
+    deixarPassageiro() : void {
+        if (this.passageiro == null) {
+            return;
+        }
+
+        if (this.passageiro.getDinheiro() < this.custo) {
+            console.log("fail: Passenger does not have enough money");
+
+            this.passageiro.setDinheiro(0);
+            console.log(this.passageiro + " leave");
+            
+            this.motorista?.setDinheiro(this.motorista?.getDinheiro() + this.custo); 
+            this.custo = 0;
+            this.passageiro = null;
+            return;
+        }
+
+        this.passageiro.setDinheiro(this.passageiro.getDinheiro() - this.custo);
+        console.log(this.passageiro + " leave");
+        
+        this.passageiro = null;
+        this.motorista?.setDinheiro(this.motorista?.getDinheiro() + this.custo); 
+        return; 
+    }
+
+    getCusto() : number {
+        return this.custo;
+    }
+
+    getMotorista() : Pessoa | null {
+        return this.motorista;
+    }
+
+    getPassageiro() : Pessoa | null {
+        return this.passageiro;
+    }
+
+    toString() : string {
+        if (this.motorista == null) {
+            return `Cost: 0, Driver: None, Passenger: None`;
+        }
+        if (this.passageiro == null) {
+            return `Cost: 0, Driver: ${this.motorista}, Passenger: None`;
+        }
+        
+        return `Cost: ${this.custo}, Driver: ${this.motorista}, Passenger: ${this.passageiro}`;
+    }
+}
+
 class Adapter {
+    moto: Moto;
+
+    constructor() {
+        this.moto = new Moto();
+    }
+
     setDriver(name: string, money: number): void {
+        this.moto.setMotorista(new Pessoa (name, money));
     }
 
     setPassenger(name: string, money: number): void {
+        this.moto.setPassageiro(new Pessoa (name, money));
     }
 
     drive(distance: number): void {
+        this.moto.dirigir(distance);
     }
 
     leavePassenger(): void {
+        this.moto.deixarPassageiro();
     }
 
     show(): void {
+        console.log(this.moto.toString());
     }
 }
 
